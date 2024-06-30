b8a20dcbfb1e06915b76146a5b18cac6aa566301
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-06-30 16:31:22 +0530

    Update sendOTP.js

--- utils/sendOTP.js
+++ utils/sendOTP.js
@@ -50,9 +50,36 @@
 // }
 
 // module.exports = sendOTPEmail;
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// // JWT_SECRET="adefrgrgrgrg"
+// const generateAuthTokenAndSetCookie = (user, res) => {
+//   const payload = {
+//     _id: user._id,
+//     username: user.username,
+//     email: user.email,
+//     dob: user.dob,
+//     universityname: user.universityname,
+//   };
+
+//   const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+//     expiresIn: "6d",
+//   });
+  
+//   res.cookie("jwt", token, {
+//     maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
+//     httpOnly: false,
+//     sameSite: "none",
+//     secure: false,
+//     // secure: process.env.NODE_ENV !== "development",
+//   });
+// };
+
+// module.exports = generateAuthTokenAndSetCookie;
+
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-// JWT_SECRET="adefrgrgrgrg"
+
 const generateAuthTokenAndSetCookie = (user, res) => {
   const payload = {
     _id: user._id,
@@ -65,13 +92,12 @@ const generateAuthTokenAndSetCookie = (user, res) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
     expiresIn: "6d",
   });
-  
+
   res.cookie("jwt", token, {
     maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
-    httpOnly: false,
+    httpOnly: true,
     sameSite: "none",
-    secure: false,
-    // secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV !== "development",
   });
 };
 
