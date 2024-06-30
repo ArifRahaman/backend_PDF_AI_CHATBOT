f14087ca02d1dea380568c75b023d1dd08af2d13
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-06-30 16:44:34 +0530

    Update generateAuthTokenAndSetCookie.js

--- utils/generateAuthTokenAndSetCookie.js
+++ utils/generateAuthTokenAndSetCookie.js
@@ -1,3 +1,28 @@
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+// // JWT_SECRET="adefrgrgrgrg"
+// const generateAuthTokenAndSetCookie = (user, res) => {
+//   const payload = {
+//     _id: user._id,
+//     username: user.username,
+//     email: user.email,
+//     dob: user.dob,
+//     universityname: user.universityname,
+//   };
+
+//   const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+//     expiresIn: "6d",
+//   });
+  
+//   res.cookie("jwt", token, {
+//     maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
+//     httpOnly: false,
+//     sameSite: "strict",
+//     // secure: process.env.NODE_ENV !== "development",
+//   });
+// };
+
+// module.exports = generateAuthTokenAndSetCookie;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 // JWT_SECRET="adefrgrgrgrg"
@@ -17,9 +42,11 @@ const generateAuthTokenAndSetCookie = (user, res) => {
   res.cookie("jwt", token, {
     maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
     httpOnly: false,
-    sameSite: "strict",
+    sameSite: "none",
+    secure: false,
     // secure: process.env.NODE_ENV !== "development",
   });
 };
 
 module.exports = generateAuthTokenAndSetCookie;
+
