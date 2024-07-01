3ad0d41bc68298772ed3e74096fbac3dce43e658
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-07-01 08:14:34 +0530

    Update generateAuthTokenAndSetCookie.js

--- utils/generateAuthTokenAndSetCookie.js
+++ utils/generateAuthTokenAndSetCookie.js
@@ -49,8 +49,8 @@ const generateAuthTokenAndSetCookie = (user, res) => {
     res.cookie("jwt", token, {
     maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
     httpOnly: false,
-    sameSite: "strict",
-    secure: false
+    sameSite: "none",
+    // secure: false
   });
 };
 
